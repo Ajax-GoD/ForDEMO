3a657d82dede2153719016630f68c9b8a29ac6a6
Ajax <ajay@demo.com>
2025-04-21 21:25:14 +0530

    removed  few lines

--- hello.py
+++ hello.py
@@ -6,9 +6,9 @@ def add_numbers(a, b):
 num1 = 5
 num2 = 7
 password = "Test123"
-API_KEY="knmgadkndadignadoigeoij3j3rj3rjrigjjwrouffdaiugOIDH9PEGUF"
+#API_KEY="knmgadkndadignadoigeoij3j3rj3rjrigjjwrouffdaiugOIDH9PEGUF"
 sum = add_numbers(num1, num2)
 
 print("The sum is:", sum)
 print(password)
-print(API_KEY)
+#print(API_KEY)
